8b33d74f30da7ca8283807651d627ba4baa9d394
Erin Stalzer <erinstalzer@gmail.com>
2019-07-24 20:20:35 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solution in this file.
+function lowerCaseDrivers(drivers) {
+    const lowCase = drivers.map(driver => driver.toLowerCase())
+    return lowCase
+}
+
+function nameToAttributes(drivers) {
+    const driversObj = drivers.map(driver =>{
+        const drivArr = driver.split(" ")
+        console.log(drivArr)
+        const drivObj = {firstName: drivArr[0], lastName: drivArr[1]}
+        console.log(drivObj)
+        return drivObj
+    }
+    return driversObj
+}
+
+function attributesToPhrase() {
+
+}
