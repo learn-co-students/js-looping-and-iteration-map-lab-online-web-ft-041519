4fdacd0e4e5eb539dce85d02a096607305417c52
Kish-Siva-1 <rodes88@msn.com>
2019-08-03 15:29:39 +0200

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solution in this file.
+
+function lowerCaseDrivers (info) {
+
+    const information = info.map(function(eng) { return eng.toLowerCase(); 
+});
+    return information;
+}
+
+function nameToAttributes (info) {
+
+    const information = info.map(function(eng) {  return Object.assign({}, {firstName: eng.split(" ", 1)[0]}
+    , {lastName: eng.split(" ", 2)[1]}) } );
+    
+    return information;
+
+}
+
+function attributesToPhrase (info) {
+
+    const information = info.map(function(eng) {  return eng.name + ' is from ' + eng.hometown } );
+    
+    return information;
+
+}
